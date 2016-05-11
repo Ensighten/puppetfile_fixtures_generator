c6eb320d4b8f0a3d48dd38bdb75955ed871f04f6
Alejandro Figueroa <alejandro@ideasftw.com>
2016-05-11 16:15:14 -0400

    Add tests for diff

--- spec/puppetfile_fixtures_generator_spec.rb
+++ spec/puppetfile_fixtures_generator_spec.rb
@@ -5,6 +5,19 @@ describe PuppetfileFixturesGenerator do
     expect(PuppetfileFixturesGenerator::VERSION).not_to be nil
   end
 
-  describe '#create_fixtures' do
+  describe '#diff and #hash_differ' do
+    let(:puppetfile) { './spec/fixtures/Puppetfile' }
+    let(:matching_diff) { './spec/fixtures/matching_diffs/.fixtures.yml' }
+    let(:nonmatching_diff) { './spec/fixtures/nonmatching_diffs/.fixtures.yml' }
+    context 'test diffs' do
+      it 'matching diff' do
+        results = PuppetfileFixturesGenerator.diff(puppetfile, matching_diff)
+        expect(results[0]).to eq(true)
+      end
+      it 'nonmatching diff' do
+        results = PuppetfileFixturesGenerator.diff(puppetfile, nonmatching_diff)
+        expect(results[0]).to eq(false)
+      end
+    end
   end
 end
